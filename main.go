00061fadf8d20c6b6c41db93d20f69c9ddc4878a
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-06-08 13:22:04 +0200

    enh: env with viper (#32)

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/joho/godotenv"
 	"github.com/primait/nuvola/cmd"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalln(err.Error())
 	}
 	cmd.Execute()
